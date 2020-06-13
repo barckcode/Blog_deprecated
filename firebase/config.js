7459b930eeae9b20ea8f3864f7558dc9b1fa92f6
BarckCode <barckcode@gmail.com>
2020-06-13 21:25:43 +0200

    Try Connect whit Firebase Databse

--- firebase/config.js
+++ firebase/config.js
@@ -1,10 +1,27 @@
-export const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
-};
+import firebase from 'firebase/app'
+import '@firebase/firestore'
 
+const loadFirebase = () => {
+  try {
+    const firebaseConfig = {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATA_BASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID,
+    }
+
+    firebase.initializeApp(firebaseConfig)
+  } catch (error) {
+    if (!/already exist/.test(error.message)) {
+      console.log('Firebase initialization error', error.stack)
+    }
+  }
+
+  return firebase
+}
+
+export default loadFirebase
--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,10 @@ import Bio from '../components/Layout/Bio'
 import Projects from '../components/Projects'
 
 //DataBase
-
+import loadFirebase from '../firebase/config'
 
 const Home = () => {
+
   return (
     <Layout>
       <Bio />
@@ -16,5 +17,33 @@ const Home = () => {
     </Layout>
   )
 }
+
+// Get Data
+// Home.getInitialProps = async () => {
+//   const firebase = await loadFirebase();
+//   const db = firebase.firestore();
+//   let result = await new Promise((resolve, reject) => {
+//     db.collection('posts')
+//       .get()
+//       .then(snapshot => {
+//         let data = [];
+//         snapshot.forEach(doc => {
+//           data.push(
+//             Object.assign(
+//               {
+//                 id: doc.id
+//               },
+//               doc.data()
+//             )
+//           );
+//         });
+//         resolve(data);
+//       })
+//       .catch(error => {
+//         reject([]);
+//       });
+//   });
+//   return { posts: result };
+// }
 
 export default Home
