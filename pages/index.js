b26a2847d145f56796866f29231ee2c61e59898b
BarckCode <barckcode@gmail.com>
2020-06-14 12:53:26 +0200

    Delete comments

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,6 @@ import Layout from '../components/Layout/Layout'
 import Bio from '../components/Layout/Bio'
 import Projects from '../components/Projects'
 
-//DataBase
-import loadFirebase from '../firebase/config'
-
 const Home = () => {
 
   return (
@@ -17,33 +14,5 @@ const Home = () => {
     </Layout>
   )
 }
-
-// Get Data
-// Home.getInitialProps = async () => {
-//   const firebase = await loadFirebase();
-//   const db = firebase.firestore();
-//   let result = await new Promise((resolve, reject) => {
-//     db.collection('posts')
-//       .get()
-//       .then(snapshot => {
-//         let data = [];
-//         snapshot.forEach(doc => {
-//           data.push(
-//             Object.assign(
-//               {
-//                 id: doc.id
-//               },
-//               doc.data()
-//             )
-//           );
-//         });
-//         resolve(data);
-//       })
-//       .catch(error => {
-//         reject([]);
-//       });
-//   });
-//   return { posts: result };
-// }
 
 export default Home
